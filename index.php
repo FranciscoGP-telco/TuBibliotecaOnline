26d0b3a5398fc83c01c368592d61478fc4ebf1f2
Francisco Garcia <apaloseko@gmail.com>
2018-11-03 15:58:54 +0100

    start to add the page events

--- index.php
+++ index.php
@@ -5,39 +5,49 @@
 	<meta charset="UTF-8">
 	<meta name="author" content="García Pozo, Francisco">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/w3.css">	
+  <link rel="stylesheet" href="css/w3.css">	
+  <link rel="stylesheet" href="css/pagedesign.css">	
 </head>
 <body>
   <main>
-<!-- Sidebar -->
-<div class="w3-sidebar w3-bar-block w3-border-right w3-animate-left" id="mySidebar">
-  <button class="w3-bar-item w3-large" id="closeButton">☰</button>
-  <a href="#" class="w3-bar-item w3-button">Link 1</a>
-  <a href="#" class="w3-bar-item w3-button">Link 2</a>
-  <a href="#" class="w3-bar-item w3-button">Link 3</a>
-</div>
+    <div class="w3-top">
+      <div class="w3-bar w3-black">
+        <a href="#" class="w3-bar-item w3-button" id="menuButton">☰</a>
+        <div id ="toNarrow">
+          <a href="#" class="w3-bar-item w3-button">tuBibliotecaOnline</a>
+          <a href="#" class="w3-bar-item w3-button">Tu biblioteca</a>
+          <a href="#" class="w3-bar-item w3-button">Libros</a>
+          <a href="#" class="w3-bar-item w3-button">Autores y editoriales</a>
+        </div>
+        <a href="#" class="w3-bar-item w3-button w3-right">usuario</a><!-- here we change this text for the user avatar (or the default one) -->
+        <a href="#" class="w3-bar-item w3-button w3-right">buscar</a><!-- here we change this text for a search button -->
+      </div>
+    </div>
 
-<!-- Page Content -->
-<div class="w3-teal">
-  <button class="w3-button w3-teal w3-xlarge" id="menuButton">☰</button>
-  <div class="w3-container">
-    <h1>My Page</h1>
-  </div>
-</div>
 
-<script>
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-}
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-}
-</script>
-     
+    <!-- Page Content -->
+    <div id="header-size"></div>
+    <!-- Sidebar -->
+    <div class="w3-sidebar w3-bar-block w3-animate-left w3-teal w3-hide" id="mySidebar">
+      <a href="#" class="w3-bar-item w3-button">Link 1</a>
+      <a href="#" class="w3-bar-item w3-button">Link 2</a>
+      <a href="#" class="w3-bar-item w3-button">Link 3</a>
+    </div>
+    <div class="w3-teal">
+      <div class="w3-container">
+        <h1>My Page</h1>
+      </div>
+    </div>
+    <div class="w3-white">
+      <div class="w3-container">
+        Anchura: <span id="w"></span>
+        Altura: <span id="h"></span>
+      </div>
+    </div>
 
-	<div class="w3-container w3-light-blue">
-		<h5>Píe de página</h5>
-	</div>
+    <div class="w3-container w3-light-blue">
+      <h5>Píe de página</h5>
+    </div>
   </main>
 	<script charset="UTF-8" src="js/menu.js"></script>
 </body>
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,34 @@
 window.onload = function() {
-  document.getElementById("closeButton").addEventListener("click", function(){
-    document.getElementById("mySidebar").style.display = "none";
-    console.log("Cierro");
-  });
+  
 
-  document.getElementById("menuButton").addEventListener("click", function(){
-    document.getElementById("mySidebar").style.display = "block";
-    console.log("Abro");
-  });
+  //Start of the events attached to the page
+  window.addEventListener("resize", topButtons);
+  document.getElementById("menuButton").addEventListener("click", sideMenu);
+
+
+  //Start of the functions for the page behaviour
+  //Function to show or hide the buttons of the top menu by its size
+  function topButtons(){
+    var windowWidth = window.innerWidth;
+    var buttons = document.getElementById("toNarrow");
+    if(windowWidth > 760){
+      buttons.classList.remove("w3-hide");
+      buttons.classList.add("w3-show");
+    } else {
+      buttons.classList.add("w3-hide");
+      buttons.classList.remove("w3-show");
+    }
+  }//end of function topButtons()
+
+  //Function to show or hide the side Menu
+  function sideMenu(){
+    var sideBar = document.getElementById("mySidebar");
+    if (sideBar.classList.contains("w3-show")){
+      sideBar.classList.add("w3-hide");
+      sideBar.classList.remove("w3-show");
+    } else {
+      sideBar.classList.add("w3-show");
+      sideBar.classList.remove("w3-hide");
+    }
+  }//End of function sideMenu()
 };
