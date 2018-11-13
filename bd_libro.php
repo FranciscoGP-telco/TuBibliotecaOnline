2ecea46524ac702cb97c5358b2aab83c006c05b7
Francisco Garcia <apaloseko@gmail.com>
2018-11-13 23:38:49 +0100

    book desing

--- bd_libro.php
+++ bd_libro.php
@@ -1,11 +1,24 @@
-    <div class="book">
+    <div class="book tbo-cream">
       <div class="w3-row">
-        <div class="w3-half w3-container tbo-cream" id="bookContainer">
-          <img src="img/9788433920423.jpg" alt="la conjura de los necios" id="bookCoverSmall">
-          <p id="booksTitle">La conjura de los necios</p>
+        <div class="w3-half w3-container">
+          <div class="bookContainer">
+            <img src="img/9788433920423.jpg" alt="la conjura de los necios" class="bookCoverSmall" id="9788433920423">
+            <p>La conjura de los necios</p>
+            <p>ISBN: 97-884-33920423</p>
+          </div>
         </div>
-        <div class="w3-half w3-container tbo-cream" id="addSearchContainer">
-          <h2>Añadir, buscar en google y tal</h2>
+        <div class="w3-half w3-container tbo-cream">
+          <div class="w3-row">
+            <div class="w3-half w3-container">
+              <button class="w3-button w3-round tbo-mint">Añadir</button>
+            </div>
+            <div class="w3-half w3-container">
+              <button class="w3-button w3-round tbo-mint">Buscar en Google</button>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="w3-row tbo-cream">
+        <p class="plot">Ignatius J. Reilly es un ser inadaptado y anacrónico que sueña con que el modo de vida medieval, así como su moral, reinen de nuevo en el mundo. Para ello, y con la intención de ser escuchado en un mundo en el que es, en realidad, un incomprendido, rellena de su puño y letra cientos de cuadernos en los que plasma su visión del mundo. Mientras llena estos cuadernos, los va desperdigando por su habitación, con la esperanza de ordenarlos algún día y así crear su ambiciosa obra maestra. Mientras, la diosa Fortuna, en contra de su voluntad, lo sume en ese mundo capitalista que él mismo tanto odia y se ve obligado a someterse a lo que él considera una forma de esclavitud: el trabajo. Resignado, se compara a sí mismo con Boecio (el cual aceptó sin queja su propia ejecución) y sale a buscar un empleo. Su actividad laboral y vital es el hilo que une y da sentido a toda la obra y lo que permite conocer a otros personajes, igual de estrambóticos y entrañables que Ignatius.</p>
       </div>
     </div>
--- php_includers/footer.php
+++ php_includers/footer.php
@@ -1,4 +1,4 @@
-  <div class="tbo-cream ">
+  <div class="tbo-cream footerContainer">
     <div class="w3-center">
       <a href="#" class="tbo-hover-grey footer" >Mapa de sitio</a> ·
       <a href="#" class="tbo-hover-grey footer" >Politica de privacidad</a> ·
