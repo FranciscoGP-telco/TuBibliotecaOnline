fd384ab6d80016c5a259e96f0c4f7ddf2ef0be0a
pakoska <apaloseko@gmail.com>
2018-12-20 10:07:03 +0100

    Update bd_publisher.php

--- body/bd_publisher.php
+++ body/bd_publisher.php
@@ -29,7 +29,7 @@
         </div>
       </div>
       <!--Scripts to charge the map in the previous div-->
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC211PskvPGvdclmu_mOafZ8zW58ijr7xw&callback=initMap"
+      <script src="https://maps.googleapis.com/maps/api/js?key=<KEY>&callback=initMap"
             async defer></script>
           <script src=js/maps.js></script>');
   } else {
